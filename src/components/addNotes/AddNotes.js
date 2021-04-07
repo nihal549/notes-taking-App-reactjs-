60a99cb2e9de8da63e862e9557bd32b5f0b1cbc9
Nihal <69518128+nihal549@users.noreply.github.com>
2021-04-07 16:53:06 +0530

    Add files via upload

---
+++ src/components/addNotes/AddNotes.js
@@ -0,0 +1,40 @@
+
+
+const AddNotes = ({ activeNote, onUpdateNote }) => {
+  const onEditField = (field, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [field]: value,
+      lastModified: Date.now(),
+    });
+  };
+
+  if (!activeNote) return <div className="no-active-note">No Active Note</div>;
+
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input
+          type="text"
+          id="title"
+          placeholder="Note Title"
+          value={activeNote.title}
+          onChange={(e) => onEditField("title", e.target.value)}
+          autoFocus
+        />
+        <textarea
+          id="body"
+          placeholder="Write your note here..."
+          value={activeNote.body}
+          onChange={(e) => onEditField("body", e.target.value)}
+        />
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+          <p className="markdown-preview">{activeNote.body}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AddNotes;
